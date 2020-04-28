a5d907b00c653a736ca213843e2d6d18a1c707cc
guyleaf <43506966+guyleaf@users.noreply.github.com>
2020-04-28 21:48:11 +0800

    finish javascript

--- js/test.js
+++ js/test.js
@@ -1,5 +1,35 @@
 $(document).ready(function(){
-    $(".button_next").addEventListener("click", function(){
+    let url = new URL(location.href);
+    let page = 1;
+    
+    if (url.searchParams.has("page"))
+    {
+        page = parseInt(url.searchParams.get("page"));
+    }
 
+    $.getJSON("json/notebook.json", function(result) {
+        $(".text").html(result[0].text[page - 1]);
+        $(".button_back").css("visibility", page != 1 ? "visible" : "hidden");
+        $(".button_next").css("visibility", result[0].text.length != page ? "visible" : "hidden");
     });
+
+    $(".button_back").on("click", function(){
+        page--;
+        $.getJSON("json/notebook.json", function(result) {
+            $(".text").html(result[0].text[page - 1]);
+            $(".button_back").css("visibility", page != 1 ? "visible" : "hidden");
+            $(".button_next").css("visibility", result[0].text.length != page ? "visible" : "hidden");
+        });
+    });
+
+    $(".button_next").on("click", function(){
+        page++;
+        $.getJSON("json/notebook.json", function(result) {
+            $(".text").html(result[0].text[page - 1]);
+            $(".button_back").css("visibility", page != 1 ? "visible" : "hidden");
+            $(".button_next").css("visibility", result[0].text.length != page ? "visible" : "hidden");
+        });
+    });
+
+    console.log("ready");
 });
