8f86fc3258e6274d4d177ed5c70977718f369807
guyleaf <43506966+guyleaf@users.noreply.github.com>
2020-04-30 12:15:13 +0800

    Modify css

--- js/journal.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-    $(".tri").hover(function(){
-        $(".Small_cir").show();
-        $('.Small_cir').css('border-color','#b9b9b9');
-    },function(){
-        $(".Small_cir").hide();
-    });
-});
